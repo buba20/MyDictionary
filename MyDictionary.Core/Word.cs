b6e02c298320addf91bf46deeda08025409b020c
buba20 <buba20@gmail.com>
2017-12-11 23:16:28 +0100

    Init

---
+++ MyDictionary.Core/Word.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary.Core
+{
+    public class Word
+    {
+        public Word(string item,string subItems)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Item = item;
+            this.SubItems = subItems;
+        }
+
+        [MongoDB.Bson.Serialization.Attributes.BsonElement]
+        public string  Id { get; set; }
+        [MongoDB.Bson.Serialization.Attributes.BsonElement]
+        public string Item { get; set; }
+        [MongoDB.Bson.Serialization.Attributes.BsonElement]
+        public string SubItems { get; set; }
+    }
+}
